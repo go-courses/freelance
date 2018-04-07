62f567813a91d18aa96b563200ffe4148440587d
Meder <medalon07@gmail.com>
2018-04-07 23:55:29 +0600

    added dirs

---
+++ migrations/00003_billings.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `billings` (
+  `id` int(11) NOT NULL,
+  `sender` int(11) NOT NULL,
+  `reciever` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `time_bill` timestamp NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `type` enum('pay','hold') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `billings`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `billings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;

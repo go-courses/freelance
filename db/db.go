1156f0075b3883e1f65b18adcd8031f831262724
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-04-14 13:55:00 +0600

    add DB interface and remove db from model

---
+++ db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import "github.com/go-courses/freelance/model"
+
+type db interface {
+	CreateUser(s model.User) (model.User, error)
+	SelectUser(id int64) (model.User, error)
+	ListUsers() ([]model.User, error)
+	UpdateUser(s model.User) (model.User, error)
+	DeleteUser(id int64) error
+
+	CreateTask(s model.Task) (model.Task, error)
+	SelectTask(id int64) (model.Task, error)
+	ListTasks() ([]model.Task, error)
+	UpdateTask(s model.Task) (model.Task, error)
+	DeleteTask(id int64) error
+
+	CreateBilling(s model.Billing) (model.Billing, error)
+	SelectBilling(id int64) (model.Billing, error)
+	ListBillings() ([]model.Billing, error)
+	UpdateBilling(s model.Billing) (model.Billing, error)
+	DeleteBilling(id int64) error
+}
--- model/db.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type db interface {
-	SelectUsers() ([]*User, error)
-	SelectTasks() ([]*Task, error)
-	SelectBillings() ([]*Billing, error)
-}
